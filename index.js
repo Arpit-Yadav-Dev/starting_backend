444634afc431e43e3a1535442220e4ea6695b3cf
Arpit Yadav <142081471+ArpitSyntaxError@users.noreply.github.com>
2024-01-29 20:48:16 +0530

    multi type request on same url with express

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const products = data.products;
 
-const server = http.createServer((req, res) => {
-  if (req.url.startsWith("/product")) {
-    const id = req.url.split("/")[2];
-    const product = products.find((p) => p.id === +id);
-    res.setHeader("Content-Type", "text/html");
-    let modifiedIndex = index
-      .replace("**title**", product.title)
-      .replace("**url**", product.thumbnail)
-      .replace("**price**", product.price)
-      .replace("**description**", product.description);
-    res.end(modifiedIndex);
-    return
-  }
+// using express server
+const express = require("express");
+const server = express();
 
-
-  switch (req.url) {
-    case "/":
-      res.setHeader("Content-Type", "text/html");
-      res.end(index);
-      break;
-    case "/api":
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(data));
-      break;
-    default:
-      res.writeHead(404);
-      res.end();
-  }
-
-  console.log("server started");
+// making a single url for differnt method of request
+// API - Endpoint - Route
+server.get("/", (req, res) => {
+  res.json({ type: "GET" });
 });
 
-server.listen(8080);
+server.post("/", (req, res) => {
+  res.json({ type: "POST" });
+});
+
+server.patch("/", (req, res) => {
+  res.json({ type: "PATCH" });
+});
+
+server.put("/", (req, res) => {
+  res.json({ type: "PUT" });
+});
+
+server.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+
+server.get("/demo", (req, res) => {
+  // res.json(products)
+  // res.sendStatus(404)
+  res.status(201).send("<h1>hello</h1>");
+  // res.sendFile('C:\\Users\\HP\\Desktop\\node-app\\index.html');
+  // in above sendfile \ is replaced by \\ because itis asking for the absolute file
+});
+
+server.listen(8080, () => console.log("Server Started"));
