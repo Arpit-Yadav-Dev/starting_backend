46378765df472cb45bbd41f027bdbf2ef8a59c76
Arpit Yadav <142081471+ArpitSyntaxError@users.noreply.github.com>
2024-01-29 22:17:35 +0530

    middlewares on different level

--- index.js
+++ index.js
@@ -6,15 +6,46 @@ const products = data.products;
 
 // using express server
 const express = require("express");
+const morgan = require("morgan");
+const { userInfo } = require("os");
 const server = express();
+
+server.use(express.json()); // for reading body
+// making my own custom middleware (application level)
+
+server.use(morgan("default")); // using the third party middleware
+server.use(express.static("public")); // this is for giving the file access from public folder files (http://localhost:8080/data.json)
+
+server.use((req, res, next) => {
+  console.log(
+    req.method,
+    req.ip,
+    req.hostname,
+    new Date(),
+    req.get("User-Agent")
+  ); //making a logger for server activities
+  next(); // use of next to move ahead from middleware otherwise it gets stuck here
+});
+
+const auth = (req, res, next) => {
+  //   if (req.body.password == 123) {
+  //     // http://localhost:8080/?password=123 for req.query.password
+  //     next();
+  //   } else {
+  //     res.sendStatus(401);
+  //   }
+  next();
+};
 
 // making a single url for differnt method of request
 // API - Endpoint - Route
-server.get("/", (req, res) => {
+server.get("/product/:id", auth, (req, res) => {
+    console.log(req.params)    // also we can get with params
+  // api level middleware (auth) 
   res.json({ type: "GET" });
 });
 
-server.post("/", (req, res) => {
+server.post("/", auth, (req, res) => {
   res.json({ type: "POST" });
 });
 
